9b4f9957e5fa5ce3b4a5de3ec22af2dd4c9589d5
Sayali2811 <sayalidhadge2811@gmail.com>
2023-04-30 09:17:07 +0530

    gallery apis added

--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,7 @@ import Gallery from "./superadmin/pages/Gallery";
 import UpdatePrivacy from "./superadmin/pages/UpdatePrivacy";
 import Slider from "./superadmin/pages/Slider";
 import Contact from "./superadmin/pages/Contact";
+import DownloadResource from "./superadmin/pages/DownloadResource";
 export default function Router() {
   const isAdmin = useSelector((state) => state.auth.admin);
   const role = useSelector((state) => state.auth.role);
@@ -51,6 +52,11 @@ export default function Router() {
           ,
           {path:"privacy",
           element:<UpdatePrivacy/>},
+          ,
+          {
+            path:'Download',
+            element:<DownloadResource/>
+          },
           {
             path:'activityType',
             element:<AddActivity/>,
--- src/superadmin/pages/Gallery.js
+++ src/superadmin/pages/Gallery.js
@@ -46,108 +46,150 @@ export default function Gallery() {
   });
 
   return (
-    <form  onSubmit={handleSubmit}>
-    <Box
-      elevation={3}
-      sx={{
-        minWidth: 345,
-        margin: "auto",
-        background: " rgba( 255, 255, 255, 0.45 )",
-        boxShadow: " 0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
-        backdropFilter: "blur( 5.5px )",
-        backdropFilter: "blur( 5.5px )",
-        borderRadius: "20px",
-        border: "1px solid rgba( 255, 255, 255, 0.18 )",
-        margin: "auto",
-        padding: "77px 99px 87px",
-        color: "#fff",
-      }}
-    >
-       <Box {...getRootProps()} sx={{ marginBottom: "2em" }}>
-        <input {...getInputProps()} />
-        <Box
-          sx={{
-            // border: "2px dashed #C4CDD5",
-            // borderRadius: "20px",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "300px",
-            width: "100%",
-            marginBottom: "1em",
-            backgroundImage: `url(${image})`,
-            backgroundSize: "contain",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-          }}
-        >
-          {!image && (
-            <>
-              <Icon
-                sx={{
-                  color: "black",
-                  width: "100%",
-                  height: "5em",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <CloudUpload sx={{ width: "3em", height: "3em" }} />
-              </Icon>
-              <Typography
-                variant="h6"
-                sx={{
-                  color: "#000",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                Upload Image here
-              </Typography>
-            </>
-          )}
+    <form onSubmit={handleSubmit}>
+      <Box
+        elevation={3}
+        sx={{
+          minWidth: 345,
+          margin: "auto",
+          background: " rgba( 255, 255, 255, 0.45 )",
+          boxShadow: " 0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
+          backdropFilter: "blur( 5.5px )",
+          backdropFilter: "blur( 5.5px )",
+          borderRadius: "20px",
+          border: "1px solid rgba( 255, 255, 255, 0.18 )",
+          margin: "auto",
+          padding: "77px 99px 87px",
+          color: "#fff",
+        }}
+      >
+        <Box {...getRootProps()} sx={{ marginBottom: "2em" }}>
+          <input {...getInputProps()} />
+          <Box
+            sx={{
+              // border: "2px dashed #C4CDD5",
+              // borderRadius: "20px",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "300px",
+              width: "100%",
+              marginBottom: "1em",
+              backgroundImage: `url(${image})`,
+              backgroundSize: "contain",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          >
+            {!image && (
+              <>
+                <Icon
+                  sx={{
+                    color: "black",
+                    width: "100%",
+                    height: "5em",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <CloudUpload sx={{ width: "3em", height: "3em" }} />
+                </Icon>
+                <Typography
+                  variant="h6"
+                  sx={{
+                    color: "#000",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  Upload Image here
+                </Typography>
+              </>
+            )}
+          </Box>
         </Box>
-      </Box>
         <Box>
-        <Typography variant="h6" gutterBottom sx={{ width: "25%",
-    borderBottom: "2px solid #B4880B",
-    color: "#003895",alignItems:'center',margin:'1em'}}>
+          <Typography
+            variant="h6"
+            gutterBottom
+            sx={{
+              width: "25%",
+              borderBottom: "2px solid #B4880B",
+              color: "#003895",
+              alignItems: "center",
+              margin: "1em",
+            }}
+          >
             Image Title
           </Typography>
           <TextField
-          required
-          fullWidth
+            required
+            fullWidth
             value={title}
             onChange={(event) => setTitle(event.target.value)}
             placeholder="Enter Title here..."
           />
         </Box>
         <Box>
-        <Typography variant="h6" gutterBottom sx={{ width: "25%",
-    borderBottom: "2px solid #B4880B",
-    color: "#003895",alignItems:'center',margin:'1em'}}>
+          <Typography
+            variant="h6"
+            gutterBottom
+            sx={{
+              width: "25%",
+              borderBottom: "2px solid #B4880B",
+              color: "#003895",
+              alignItems: "center",
+              margin: "1em",
+            }}
+          >
             Image Description
           </Typography>
           <TextField
-           required
-          fullWidth
+            required
+            fullWidth
             value={description}
             onChange={(event) => setDescription(event.target.value)}
             placeholder="Enter description here..."
           />
         </Box>
-        <Button
+        <Box sx={{display:'flex',justifyContent:'space-around',flexDirection:'row'}}> <Button
           type="submit"
           variant="contained"
-          sx={{ width:'120px',color: "#FFF", backgroundColor: "#1D3D7C", padding: "10px",
-        display:'flex',justifyContent:'center',margin:'auto',fontSize:'1.12em',marginTop:'1em' }}
+          sx={{
+            width: "120px",
+            color: "#FFF",
+            backgroundColor: "#1D3D7C",
+            padding: "10px",
+            display: "flex",
+            justifyContent: "center",
+            margin: "auto",
+            fontSize: "1.12em",
+            marginTop: "1em",
+          }}
         >
           Save
         </Button>
+        <Button
+          type="submit"
+          variant="contained"
+          sx={{
+            width: "120px",
+            color: "#FFF",
+            backgroundColor: "#1D3D7C",
+            padding: "10px",
+            display: "flex",
+            justifyContent: "center",
+            margin: "auto",
+            fontSize: "1.12em",
+            marginTop: "1em",
+          }}
+        >
+          Delete
+        </Button></Box>
       </Box>
-      </form>
-    );
-  }
+    </form>
+  );
+}
